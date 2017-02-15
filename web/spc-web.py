c37daaeb9d24a64e7ebc4b310235790118be245c
Igor Korostil <igork@t-e-g.com.au>
2017-02-15 17:43:56 +1100

    more web

--- web/spc-web.py
+++ web/spc-web.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, redirect
+from flask_wtf import Form
+from .forms import ContactForm
 
 app = Flask(__name__)
 
 @app.route("/testing", methods=['GET','POST'])
 def show_form():
 	
-	form = LoginForm()
+	form = ContactForm()
 
 	if request.method == 'POST':
 		return render_template('testing.html', form = form)
