00b3e47cb9706b2ff587abc177f424be5f02971e
Igor Korostil <igork@t-e-g.com.au>
2017-02-16 08:11:50 +1100

    basic works

--- web/spc-web.py
+++ web/spc-web.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from flask import request, render_template, redirect
 from flask_wtf import Form
-from .forms import ContactForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+# make a new class (which is effectively the form) based on the base class Form
+class SportStringForm(Form):
+	descr_str = StringField(u'event description', validators=[DataRequired()])
 
 @app.route("/testing", methods=['GET','POST'])
 def show_form():
 	
-	form = ContactForm()
+	form = SportStringForm()
 
-	if request.method == 'POST':
-		return render_template('testing.html', form = form)
+	if request.method == "POST":
+		st = request.form["descr_str"]
+		print(st)
+	return render_template('sports_text.html', form = form)
+
 
 if __name__== "__main__":
 	app.run(debug=True)
