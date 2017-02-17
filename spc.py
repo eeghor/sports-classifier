afb7d663d695d78a9aaf7599c46307951f6f973d
Igor Korostil <igork@t-e-g.com.au>
2017-02-17 17:51:20 +1100

    updated

--- spc.py
+++ spc.py
@@ -1,16 +1,19 @@
 from file_fncs import file2list, list2file, setup_dirs, enc_lst, dec_dict
 import pandas as pd
 import re
+import numpy as np
 
 from collections import defaultdict, Counter
 import time
 from datetime import datetime as dt
+import itertools
 
-from sklearn.ensemble import RandomForestClassifier
 
-import numpy as np
-import sys, os, shutil
-from sklearn import svm
+#from sklearn.ensemble import RandomForestClassifier
+
+
+#import sys, os, shutil
+#from sklearn import svm
 
 import multiprocessing_on_dill as multiprocessing
 from multiprocessing_on_dill import Pool
@@ -229,7 +232,7 @@ class SportsClassifier(object):
 			for nickname_line in self.AUS_TEAM_NICKS:
 				nick = nickname_line.split("-")[0].strip()
 				sport = nickname_line.split("-")[1].strip()
-				if nick in st:
+				if nick and nick in st:
 					st_features["@{}_NICKNAME".format(sport)] += 1
 
 			# theatre company features
@@ -262,382 +265,128 @@ class SportsClassifier(object):
 			for sport in self.sports:
 				if sport.replace("_"," ") in st:
 					st_features["@SPORT_NAME_{}".format(sport)] += 1
+
+
 		return st_features
 
+	def extract_features_from_df(self, df):
 
+		di = defaultdict(lambda: defaultdict(int))
+
+		# create a corpus
+		from sklearn.feature_extraction.text import CountVectorizer
+		from sklearn.feature_extraction.text import TfidfTransformer
+		from sklearn.feature_extraction.text import TfidfVectorizer
+
+		transformer = TfidfTransformer(smooth_idf=False)
 		
-		
-		
-		# for sport in self.venue_names:
-		# 	for comp in self.venue_names[sport]:
-		# 		st = self.__prelabel_from_list(st, self.venue_names[sport][comp], u"_SPORTS_VENUE_", 2)
-		# #print("after pre-labelled venue names:",st) 
+		corpus = []
 
-		# st = self.__prelabel_from_list(st, self.AUS_THEATRE_COMPANIES, u"_THEATRE_COMPANY_", 2)
-		# st = self.__prelabel_from_list(st, self.AUS_OPERA_COMPANIES, u"_OPERA_COMPANY_", 2)
-		# # st = self.__prelabel_from_list(st, self.COUNTRIES, u"_COUNTRY_", 1)
-		# # st = self.__prelabel_from_list(st, self.AUS_SUBURBS, u"_AUS_LOCATION_", 1)
+		for st in itertools.chain.from_iterable([df["event"], df["venue"]]):
+			corpus.append(st.lower())
 
-		# st = self.__prelabel_from_list(st, self.PERFORMERS, u"_ARTIST_", 2)
-		
-		# # st = self.__prelabel_from_list(st, self.NAMES_M, u"_NAME_", 1)
+		bigram_vectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=1)
+		X = bigram_vectorizer.fit_transform(corpus).toarray()
 
-		# # remove stopwords
-		# st = " ".join([w for w in st.split() if w not in self.STPW])
-		
-		# st = re.compile(r"(?<!\w)\w*\d+\w*(?!\w+)").sub('', st)
+		for i, s in enumerate(df.itertuples()):  
+			full_descr = s.event + " " + s.venue
+			if full_descr.strip():
+				di[s.Index].update(self.extract_features(full_descr))
 
-		# # remove the multiple white spaces again
-		# st = re.sub(r"\s+"," ",st)
+		# columns are event | venue | month | weekday | sport
 
-		# return st	
-
-	def normalize_df(self, df):
-
-		df["event"] = df["event"].str.lower().str.split().str.join(" ")
-		df["event"] = df["event"].str.replace("."," ")
-		df["venue"] = df["venue"].str.lower().str.split().str.join(" ")
-		df["event"] = [_event.replace(_venue,"") for _event, _venue in zip(df["event"],df["venue"])]
-
-		#print("event BEFORE normalize string:",df.iloc[:4,0])
-		df['event'] = df['event'].apply(lambda x: self.normalize_string(x))		
-		#print("event AFTER normalize string:",df.iloc[:4,0])
-
-		return df
-
-
-	def parallelize_dataframe(self, df, func):
-
-   		df_split = np.array_split(df, 8)
-   		pool = Pool(8)
-   		rr = pool.map(func, df_split)
-   		df = pd.concat(rr)
-   		pool.close() #  informs the processor that no new tasks will be added to the pool
-   		pool.join()  # stops and waits for all of the results to be finished and collected before proceeding with the rest of the program
-
-   		return df
-
-
+		return di
+			
 	@run_and_time
-	def normalize_data(self, df, k, para="no"):
+	def extract_features_from_df_parallel(self, df, nworkers=8):
 
-		print("[normalising {} data]".format(k))
-
-		if para == "yes":
-			print("note: you chose to run normalisation in parallel")
-			df = self.parallelize_dataframe(df, self.normalize_df)	
-		elif para == "no":
-			df = self.normalize_df(df)
-			print("note: you chose not to run normalisation in parallel")
-		else:
-			sys.exit("please, choose yes or no regarding the parallelisation option.")
-
-		if k == "training":
-
-			for col in ['event', 'venue']:
-				for st_as_lst in df[col].str.split():
-					for w in st_as_lst:
-							self.train_dict[w] += 1
-
-			self.words_once = [w for w in self.train_dict if self.train_dict[w] < 8]
-
-			print("found {} words that occurr only once ({}%)...".format(len(self.words_once), round(len(self.words_once)/len(self.train_dict)*100.0),1))
-			
-			self.list2file(self.rare_words_file, self.words_once)
-
-			self.train_word_list = [str(w) for w in self.train_dict if w not in self.words_once]
-
-			print("words to be used to extract features: {}".format(len(self.train_word_list)))
-		else:
-			pass
-		for col in ['event', 'venue']:
-			df[col] = df[col].apply(lambda x: " ".join([w for w in x.split() if  w in self.train_word_list]))
-		#print("event AFTER only popular words:",df.iloc[:4,0])
-
-		df.to_csv(self.TEMP_DATA_DIR + "normalised_" + k + "_df.csv")
-
-		return df
-
-	def getf_special_word(self, st):
-
-		res_dict = defaultdict()  # to keep collected features in
-
-		for sport in self.team_name_words:
-
-			in_both_sets = set(self.team_name_words[sport]) & set(st.lower().split()) 
-
-			if in_both_sets:
-
-				res_dict["@" + sport.upper() + "_team_words"] = len(in_both_sets)
-
-		for sport in self.comp_name_words:
-
-			in_both_sets = set(self.comp_name_words[sport]) & set(st.lower().split()) 
-
-			if in_both_sets:
-
-				res_dict["@" + sport.upper() + "_comp_words"] = len(in_both_sets)
-
-		return res_dict
-
-
-	def getf_1g(self, st):
-
-		c = Counter(st.split())
-
-		return {"@word_[{}]".format(w.strip()): c[w] for w in c}
-
-	def getf_2g(self, st):
-
-		res_dict = defaultdict(int)
+		print("extracting features...")
 		
-		str_list = st.split()
-		
-		if len(str_list) > 1:
-			for i, w in enumerate(str_list):
-				if i > 0:
-					res_dict[("@words_[{}]->[{}]").format(str_list[i-1], w)] += 1
+		df_split = np.array_split(df, nworkers)
+		pool = Pool(nworkers)
+		res_dicts = pool.map(self.extract_features_from_df, df_split)
+		pool.close() #  informs the processor that no new tasks will be added to the pool
+		pool.join()  # stops and waits for all of the results to be finished and collected before proceeding with the rest of 
 
-		return res_dict
+		big_dic = defaultdict(lambda: defaultdict(int))
 
-	def getf_isvs(self, st):
+		# merge feature dictionaries created for data frame splits into one big dictionary
+		for dic in res_dicts:
+			for k,v in dic.items():
+				big_dic[k] = v
 
-		if {"vs","vs.","v"} & set(st.split()):
-			return {"@word_[vs]_present": 1}
-		else:
-			return {"@word_[vs]_present": 0}
-
-	def getf_sportname(self, st):
-
-		res_dict = defaultdict(int)
-
-		for sport in "afl soccer nrl rugby_union basketball netball tennis horse_racing".split():
-			if re.sub("_", " ", sport) in st:
-				res_dict["@sport_[{}]_mentioned".format(sport.upper())] += 1
-
-		return res_dict 
-
-	def getf_nicks(self, st):
-
-		res_dict = defaultdict(int)
-
-		for n in self.AUS_TEAM_NICKS:	
-
-			nick = n.split("-")[0].strip()
-			sport = n.split("-")[1].strip()
-
-			if nick in st:
-				res_dict["@nick_[{}]_present".format(sport.upper())] += 1
-		return res_dict
-
-	def getf_countries(self, st):
-
-		res_dict = defaultdict(int)
-
-		for country in self.COUNTRIES:	
-			if country in st:
-				res_dict["@country"] += 1
-
-		return res_dict
-	
-	def getf_names(self, st):
-
-		res_dict = defaultdict(int)
-
-		for name in self.NAMES_M:	
-			if re.search("(?<!\w)name(?!\w)",st):
-				res_dict["@name"] += 1
-
-		return res_dict
-
-
-	def getf_burbs(self, st):
-
-		res_dict = defaultdict(int)
-
-		for suburb in self.AUS_SUBURBS:
-			if suburb in st:
-				res_dict["@city_suburb"] += 1
-
-		return res_dict
-
-	def getf_month(self, month_number):
-
-		return {"@month_[{}]".format(month_number): 1}
-
-	def getf_upper(self, st):
-
-		res_dict = defaultdict(int)
-
-		for w in st.split():
-			if w.isupper():
-				res_dict[w] += 1
-
-		return res_dict
-
-	# def getf_timeofday(self, hour):
-
-	# 	if (int(hour) >= 1) and (int(hour) < 12):
-	# 		time_of_day = "morning"
-	# 	elif  (int(hour) >= 12) and (int(hour) < 18):
-	# 		time_of_day = "afternoon"
-	# 	else:
-	# 		time_of_day = "evening"
-
-	# 	return {"event_timeofday_[{}]".format(time_of_day): 1}
-
-
-	@run_and_time
-	def get_features(self, d, k):
-
-		"""
-		extracts features from a data frame
-		"""
-		
-		di = defaultdict(lambda: defaultdict(int))  # will keep extracted features here	
-		
-		print("[extracting {} features]".format(k))
-
-
-		for i, s in enumerate(d.itertuples()):  #  go by rows
-			
-			pk = s.Index  # recall that pks are data frame index
-			
-			di[pk].update(self.getf_special_word(s.event))
-			di[pk].update(self.getf_nicks(s.event))
-			#di[pk].update(self.getf_timeofday(s.hour))
-			di[pk].update(self.getf_1g(s.event))
-			di[pk].update(self.getf_2g(s.event))
-			di[pk].update(self.getf_isvs(s.event))
-			di[pk].update(self.getf_sportname(s.event))
-			di[pk].update(self.getf_burbs(s.event))
-			di[pk].update(self.getf_countries(s.event))
-			di[pk].update(self.getf_names(s.event))
-			di[pk].update(self.getf_upper(s.event))
-			di[pk].update(self.getf_month(s.month))
-	
-		# merge the original data frame with a new one created from extracted features to make one feature data frame
-
-	
-		fdf = pd.concat([d[d.columns.difference(["event", "venue","month"])], 
-						pd.DataFrame.from_dict(di, orient='index')], axis=1, join_axes=[d.index]).fillna(0)
-
-
-		if k == "training":
-
-			print("chekcing indexes:")
-
-			if d.index.equals(self.y_train.index):
-
-				print("yes, the training set index is OK")
-			else:
-				print("index of y_train:",self.y_train.index)
-				print("index of d:",d.index)
-				print("difference: what is in d but not in y-train:", set(list(d.index.values)) - set(list(self.y_train.index.values)))
-				sys.exit("WRONG INDEX in trainng set!!")
-			
-			self.model_feature_names = list(fdf)
-			print("total model features...{}...ok".format(len(self.model_feature_names)))
-		
-			self.list2file(self.model_fnames, self.model_feature_names)
-
-
-			ff = pd.concat([d,pd.DataFrame.from_dict(di, orient='index'),self.y_train.apply(lambda _: self.sports_decoded[_])], axis=1, join_axes=[d.index]).fillna(0)
-			ff.to_csv(self.TEMP_DATA_DIR + "training_w_features.csv")
-		
-		
-		
-
-
-		elif k == "testing":
-			"""
-			now we ignore features that are not in self.model_feature_names
-			"""
-
-			print("checking indexes:")
-			if d.index.equals(self.y_test.index):
-				print("yes, the testing set index is OK")
-			else:
-				sys.exit("WRONG INDEX in testing set!!")
-
-			fdf.drop(fdf.columns.difference(self.model_feature_names), axis=1, inplace=True)
-
-			for fch in self.model_feature_names:
-				if fch not in fdf.columns:
-					fdf[fch] = 0
-
-			pd.concat([d,pd.DataFrame.from_dict(di, orient='index'),self.y_test.apply(lambda _: self.sports_decoded[_])], axis=1, join_axes=[d.index]).fillna(0).to_csv(self.TEMP_DATA_DIR + "testing_w_features.csv")
-
-		return  fdf   # returns new data frame that has feature columns attached
+		return pd.concat([pd.get_dummies(df[df.columns.difference(["event", "venue"])], prefix="@", columns=["month","weekday"]), pd.DataFrame.from_dict(big_dic, orient='index')], axis=1, join_axes=[df.index]).fillna(0.)
 
 	def select_features(self, feature_df):
 
 		from sklearn.feature_selection import VarianceThreshold
 		print("before variance threshold have {} features".format(feature_df.shape[1]))
 		selekt = VarianceThreshold(threshold=.8*(1.-.8))
+		#print(pd.isnull(feature_df))
+
 		new_feature_df = selekt.fit_transform(feature_df)
 		print("after variance threshold have {} features".format(new_feature_df.shape[1]))
-		print(new_feature_df.head())		
+		print(new_feature_df)
 
 		return new_feature_df
 
 
 
 
-	@run_and_time
-	def run_classifier(self):
+	# @run_and_time
+	# def run_classifier(self):
 
-		print("[training model]")
+	# 	print("[training model]")
 
-		from sklearn.multiclass import OneVsRestClassifier
-		from sklearn.svm import LinearSVC
-		from sklearn.metrics import accuracy_score
-		from sklearn.model_selection import GridSearchCV
+	# 	from sklearn.multiclass import OneVsRestClassifier
+	# 	from sklearn.svm import LinearSVC
+	# 	from sklearn.metrics import accuracy_score
+	# 	from sklearn.model_selection import GridSearchCV
 
-		#classifier = OneVsRestClassifier(LinearSVC(random_state=0))
-		forest_parameters = {"max_features": [None, "log2"],
-							"n_estimators": [200,300,500],
-							"n_jobs":[2,-1],
-							"max_depth":[2]}
+	# 	#classifier = OneVsRestClassifier(LinearSVC(random_state=0))
+	# 	forest_parameters = {"max_features": [None, "log2"],
+	# 						"n_estimators": [200,300,500],
+	# 						"n_jobs":[2,-1],
+	# 						"max_depth":[2]}
 
 
-		forest = RandomForestClassifier()
-		#forest = RandomForestClassifier(max_features=None, n_estimators=200,n_jobs=-1)
+	# 	forest = RandomForestClassifier()
+	# 	#forest = RandomForestClassifier(max_features=None, n_estimators=200,n_jobs=-1)
 	
-		#classifier = OneVsRestClassifier(RandomForestClassifier(n_estimators=50))
+	# 	#classifier = OneVsRestClassifier(RandomForestClassifier(n_estimators=50))
 
-		classifier = GridSearchCV(forest, forest_parameters)
+	# 	classifier = GridSearchCV(forest, forest_parameters)
 
-		classifier = forest
+	# 	classifier = forest
 
-		print("chekcing indexes in PREDICTION:")
-		if self.train.index.equals(self.y_train.index):
-			print("yes, the training set index is OK")
-		else:
-			sys.exit("WRONG INDEX in trainng set!!")
+	# 	print("chekcing indexes in PREDICTION:")
+	# 	if self.train.index.equals(self.y_train.index):
+	# 		print("yes, the training set index is OK")
+	# 	else:
+	# 		sys.exit("WRONG INDEX in trainng set!!")
 
-		classifier.fit(self.train, self.y_train)
+	# 	classifier.fit(self.train, self.y_train)
 
-		dill.dump(classifier, open("trained_model.dill","w"))
+	# 	dill.dump(classifier, open("trained_model.dill","w"))
 
-		pred_train = pd.Series(classifier.predict(self.train), index=self.train.index, name="predicted_sport")
-		print("real:",self.y_train[:5])
-		print("predincted:",pred_train[:5])
+	# 	pred_train = pd.Series(classifier.predict(self.train), index=self.train.index, name="predicted_sport")
+	# 	print("real:",self.y_train[:5])
+	# 	print("predincted:",pred_train[:5])
 
-		print("TRAINING accuracy:", round(accuracy_score(self.y_train, pred_train),2))
+	# 	print("TRAINING accuracy:", round(accuracy_score(self.y_train, pred_train),2))
 
 
-		y_pred = pd.Series(classifier.predict(self.test), index=self.test.index, name="sport_p")
+	# 	y_pred = pd.Series(classifier.predict(self.test), index=self.test.index, name="sport_p")
 
-		print("real:", self.y_test[:5])
-		print("predicted:",y_pred[:5])
+	# 	print("real:", self.y_test[:5])
+	# 	print("predicted:",y_pred[:5])
 
-		pd.concat([self.test_nofeatures,self.test,self.y_test.apply(lambda _: self.sports_decoded[_]),y_pred.apply(lambda _: self.sports_decoded[_])], axis=1).fillna(0).to_csv(self.TEMP_DATA_DIR + "testing_report.csv")
+	# 	pd.concat([self.test_nofeatures,self.test,self.y_test.apply(lambda _: self.sports_decoded[_]),y_pred.apply(lambda _: self.sports_decoded[_])], axis=1).fillna(0).to_csv(self.TEMP_DATA_DIR + "testing_report.csv")
 
-		print("TESTING accuracy:", round(accuracy_score(self.y_test, y_pred),2))
-		# .apply(lambda _: self.sports_decoded[_]
-		# save the predictions to file
-		pd.concat([self.test, y_pred], axis=1, join="inner").to_csv(self.predictions_test_file)
+	# 	print("TESTING accuracy:", round(accuracy_score(self.y_test, y_pred),2))
+	# 	# .apply(lambda _: self.sports_decoded[_]
+	# 	# save the predictions to file
+	# 	pd.concat([self.test, y_pred], axis=1, join="inner").to_csv(self.predictions_test_file)
 
 		
 
@@ -650,6 +399,10 @@ if __name__ == '__main__':
 	cl.make_train_test()
 
 	print(cl.extract_features("st kilda recently bought a defender from sydney fc but $50,000 was not cheap for an a-league player; kookaburras however travelled to China - clearly, Anita sent Amit away with that contract, as Josh confirmed to Alex --- India v Sri LAnka today, then followed by Russian Federation vs. USA; another rugby union team here"))
+
+	dfx = cl.extract_features_from_df_parallel(cl.train_nofeatures)
+	print(dfx.columns.values)
+	new_dfx = cl.select_features(dfx)
 
 	# x_tr = cl.normalize_data(cl.train_nofeatures, "training", para="yes")
 
